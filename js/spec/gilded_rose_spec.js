3c03843483f6b7af76f8f4db2e2599794e19a46b
Lewis Wakeland <Lewis.Wakeland@softwire.com>
2018-07-04 12:18:08 +0100

    Refactored code

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,82 +1,90 @@
-describe("Gilded Rose", function() {
+describe("Gilded Rose", function () {
 
-  it("should not allow the qulity of an item to be negative", function() {
-    const gildedRose = new Shop([ new Item("negative", 1, -1) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBeGreaterThan(-1);
+  it("should not allow the qulity of an item to be negative", function () {
+    let item = new Item("any item", 1, -1);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBeGreaterThan(-1);
   });
 
-  it("should not allow the qulity of an item to become negative", function() {
-    const gildedRose = new Shop([ new Item("negative", 1, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBeGreaterThan(-1);
+  it("should not allow the qulity of an item to become negative", function () {
+    let item = new Item("any item", 1, 0);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBeGreaterThan(-1);
   });
 
-  it("should increase the quality of Aged Brie as it gets older", function() {
-    const gildedRose = new Shop([ new Item("Aged Brie", 1, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBeGreaterThan(0);
+  it("should increase the quality of Aged Brie as it gets older", function () {
+    let item = new Item(propertyNames.agedBrie, 1, 0);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBeGreaterThan(0);
   });
 
-  it("should not allow the quality of an item to be greater than 50", function() {
-    const gildedRose = new Shop([ new Item("Aged Brie", 1, 60) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBeLessThan(51);
+  it("should not allow the quality of an item to be greater than 50", function () {
+    let item = new Item(propertyNames.agedBrie, 1, 60);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBeLessThan(51);
   });
 
-  it("should not allow the quality of an item to become greater than 50", function() {
-    const gildedRose = new Shop([ new Item("Aged Brie", 1, 50) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBeLessThan(51);
+  it("should not allow the quality of an item to become greater than 50", function () {
+    let item = new Item(propertyNames.agedBrie, 1, 50);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBeLessThan(51);
   });
 
-  it("should not change the sell by date of Sulfuras", function() {
-    const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 1, 20) ]);
+  it("should not change the sell by date of Sulfuras", function () {
+    let item = new Item(`${propertyNames.sulfuras}, Hand of Ragnaros`, 1, 20);
+    const gildedRose = new Shop([item]);
     const items = gildedRose.updateQuality();
-    expect(items[0].sellIn).toBe(1);
+    const sellIn = items[0].sellIn;
+    expect(sellIn).toBe(1);
   });
 
-  it("should not decrease the quality of Sulfuras", function() {
-    const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 1, 20) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(20);
+  it("should not decrease the quality of Sulfuras", function () {
+    let item = new Item(`${propertyNames.sulfuras}, Hand of Ragnaros`, 1, 20);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBe(20);
   });
 
-  it("should increase the quality of Backstage Passes", function() {
-    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKBL90ETC concert", 20, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(1);
+  it("should increase the quality of Backstage Passes", function () {
+    let item = new Item(`${propertyNames.backstagePass}es to a TAFKBL90ETC concert`, 20, 0);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBe(1);
   });
 
-  it("should increase the quality of Backstage Passes by 2 when there are 10 days or less before the concert", function() {
-    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKBL90ETC concert", 7, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(2);
+  it("should increase the quality of Backstage Passes by 2 when there are 10 days or less before the concert", function () {
+    let item = new Item(`${propertyNames.backstagePass}es to a TAFKBL90ETC concert`, 7, 0);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBe(2);
   });
 
-  it("should increase the quality of Backstage Passes by 3 when there are 5 days or less before the concert", function() {
-    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKBL90ETC concert", 3, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(3);
+  it("should increase the quality of Backstage Passes by 3 when there are 5 days or less before the concert", function () {
+    let item = new Item(`${propertyNames.backstagePass}es to a TAFKBL90ETC concert`, 3, 0);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBe(3);
   });
 
-  it("should decrease the quality of Backstage Passes to 0 after the concert", function() {
-    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKBL90ETC concert", -1, 10) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(0);
+  it("should decrease the quality of Backstage Passes to 0 after the concert", function () {
+    let item = new Item(`${propertyNames.backstagePass} to a TAFKBL90ETC concert`, -1, 10);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBe(0);
   });
 
-  it("should decrease the quality of items twice as fast once their sell by date has passed", function() {
-    const gildedRose = new Shop([ new Item("bar", -1, 5) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(3);
+  it("should decrease the quality of items twice as fast once their sell by date has passed", function () {
+    let item = new Item("any item", -1, 5);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBe(3);
   });
 
 
-  it("Conjured should decrease x2", function() {
-    const gildedRose = new Shop([ new Item("Conjured bar", -1, 5) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(1);
+  it("Conjured should decrease x2", function () {
+    let item = new Item(`${propertyNames.conjured} any item`, -1, 5);
+    const quality = getQualityAfterUpdatingItem(item);
+    expect(quality).toBe(1);
   });
 
 });
+
+function getQualityAfterUpdatingItem(item) {
+  const gildedRose = new Shop([item]);
+  const items = gildedRose.updateQuality();
+  return items[0].quality;
+}
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -6,6 +6,28 @@ class Item {
   }
 }
 
+let propertyNames = {
+  sulfuras: "Sulfuras",
+  backstagePass: "Backstage pass",
+  conjured: "Conjured",
+  agedBrie: "Aged Brie"
+}
+
+class ItemProperties {
+  constructor(item) {
+    this.name = item.name;
+    this.isSulfuras = item.name.startsWith(propertyNames.sulfuras);
+    this.isBackstagePass = item.name.startsWith(propertyNames.backstagePass);
+    this.isConjured = item.name.startsWith(propertyNames.conjured);
+    //Copies name
+    let tempName = (' ' + item.name).slice(1);
+    if (this.isConjured) {
+      tempName = tempName.slice(9);
+    }
+    this.isAgedBrie = (tempName === propertyNames.agedBrie);
+  }
+}
+
 class Shop {
   constructor(items = []) {
     this.items = items;
@@ -13,53 +35,73 @@ class Shop {
   updateQuality() {
 
     for (var i = 0; i < this.items.length; i++) {
+
       let item = this.items[i];
-      if (item.name.startsWith('Sulfuras')) {
-        continue;
-      }
 
-      
-      let qualityChangeAmount = -1;
-
-      let increasingQuality = item.name === 'Aged Brie' || item.name.startsWith('Backstage pass');
-      if (increasingQuality) {
-        qualityChangeAmount = 1;
-        if (item.name.startsWith('Backstage pass')) {
-          qualityChangeAmount = this.updateBackstagePassQuality(item);
-        }
-      }
-
-      item.sellIn = item.sellIn - 1;
-
-      if (item.sellIn < 0) {
-          qualityChangeAmount=qualityChangeAmount*2;
-      }
-
-      if(item.name.startsWith('Conjured')){
-         qualityChangeAmount*=2;
-      }
-      item.quality += qualityChangeAmount;
-      //Prevent the quality from boing below 0 or becoming greater then 50
-      if (item.quality <= 0) {
-        item.quality = Math.max(0, item.quality);
-      } else if (item.quality >= 50) {
-        item.quality = Math.min(50, item.quality);
-      }
+      this.changeItemQuality(item);
     }
 
     return this.items;
+
+  }
+  changeItemQuality(item) {
+    let properties = new ItemProperties(item);
+    //Sulfuras never change
+    if (properties.isSulfuras) {
+      return;
+    }
+
+    //default multiplier
+    let multiplier = 1;
+    item.sellIn = item.sellIn - 1;
+
+    if (item.sellIn < 0) {
+      multiplier *= 2;
+    }
+    if (properties.isConjured) {
+      multiplier *= 2;
+
+    }
+
+    //defult quality change
+    let qualityChangeAmount = -1;
+
+    if (properties.isAgedBrie) {
+      qualityChangeAmount = 1;
+    }
+    if (properties.isBackstagePass) {
+      qualityChangeAmount = this.getBackstagePassQualityChange(item);
+    }
+
+    qualityChangeAmount *= multiplier;
+
+    item.quality += qualityChangeAmount;
+
+    //Prevent the quality from boing below 0 or becoming greater then 50
+    this.boundQuality(item);
+
   }
 
-  updateBackstagePassQuality(item) {
-    if(item.sellIn<0){
+  getBackstagePassQualityChange(item) {
+    /*- "Backstage passes" increases in Quality as its SellIn value approaches;
+	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+  Quality drops to 0 after the concert*/
+
+    if (item.sellIn < 0) {
       return -item.quality;
-    }else if (item.sellIn < 6) {
+    } else if (item.sellIn <= 5) {
       return 3;
-    }else if (item.sellIn < 11) {
+    } else if (item.sellIn <= 10) {
       return 2;
-    }else{
+    } else {
       return 1;
     }
   }
 
+  boundQuality(item) {
+    item.quality = Math.max(0, item.quality);
+    item.quality = Math.min(50, item.quality);
+  }
+
 }
+
